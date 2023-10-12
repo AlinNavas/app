a98eab03a68771813d94ca1917e95a4918786161
alinnavas <alin26navas@gmail.com>
2023-10-12 10:53:57 +0100

    d

--- Home_Page.py
+++ Home_Page.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import streamlit_extras
 st.set_option('deprecation.showPyplotGlobalUse', False)
-
+from PIL import Image
 
 
 with open("img.png", "rb") as f:
@@ -21,51 +21,81 @@ with open("img.png", "rb") as f:
     )
 
     st.sidebar.header("Diabetes Predictor")
-    st.sidebar.markdown("I developed a screening tool while I was in medical school that can predict the presence of diabetes based on symptoms exhibited when a patient is in a hyperglycaemic state using ML XYZ methods.")   
+    st.sidebar.markdown("I developed a screening tool while I was in medical school that can predict the presence of diabetes based on symptoms exhibited when a patient is in a hyperglycaemic state using Support Vector Machine ML method.")   
     st.sidebar.markdown("Developed by: Dr. Alin Navas")                   
 
-import streamlit.components.v1 as components
-p = open("trial.htm",'r',encoding="ISO-8859-1")
-components.html(p.read())
-
-header = st.container()
-chart = st.container()
-ii_para =st.container()
-
-with header:
-    st.title('Predicting diabetic risk')
-    st.markdown('The global prevalence of diabetes mellitus in 2019 was around 9.3% which is estimated to rise to 10.2% by 2030 and 10.9% by 2045. This future spike in diabetes can be attributed to a number of factors including the rising standard of living in developing nations which is generally associated with increased availability and consumption of processed foods and transitioning to sedentary lifestyles. ')
 
 
-with chart:
-
-    df= pd.read_csv("data/idf_count_diabetes.csv")
-    st.markdown("View the increase in diabetic population in different region by selecting the year of choice")
-    year = st.selectbox('Select Year',df.columns[3:8])
+body = st.container()
 
 
-    plt.figure(figsize=(6,6))
-    plt.bar(df['Region'],df[year])
-    plt.xlabel('Region')
-    plt.ylabel('Diabetic population')
-    plt.xticks(rotation=30,fontsize=10,ha="right")
-    plt.yticks(range(0,280000,20000))
-    plt.title(f'No of individuals with diabetes in {year}')
-    plt.tight_layout()
-    st.pyplot(plt)
 
-with ii_para:
+with body:
+    st.markdown("<h1 style='text-align: center; color: SkyBlue;'>Predict your risk of having diabetes!</h1>", unsafe_allow_html=True)
+    st.markdown('  ')
+    st.markdown('  ')
+    st.markdown('Studies have shown that a significant number of people who are diabetic are unaware of their condition. Even developed countries with high per capita expenditure on healthcare, like the US, have around 8.5 million adults with undiagnosed diabetes. This represents nearly 23 percent of all US adults with diabetes. ')
+    st.markdown('On a global scale, some papers claim that 1 in 2 people living with diabetes don’t know that they have it.  ')
+    st.markdown('This lack of awareness can be very dangerous. Uncontrolled diabetes mellitus may lead to many serious complications like the ones shown below:')
+    st.markdown('  ')
+    st.markdown('1)	Diabetic retinopathy : The development and progression of DR is primarily caused by the tissue-damaging effects of chronic hyperglycaemia that results in a complex interplay of multiple mechanisms ,which cause two basic changes within the retinal vessels, namely: abnormal permeability, and occlusion with ischemia and subsequent neovascularization.  ')
+    image = Image.open('trial_files/image001.jpg')
+    col1, col2, col3 = st.columns(3)
+    col2.image(image, caption='Fundus of a patient with severe diabetic retinopathy')
+    st.markdown('2)	Diabetic nephropathy : Hyperglycaemia results in production of advanced glycation end-products (AGE) and reactive oxygen species. These aberrant metabolic products activate intercellular signaling for proinflammatory and profibrotic gene expression with production of a host of mediators for cellular injury. ')
+    image2 = Image.open('trial_files/image002.jpg')
+    col1, col2, col3 = st.columns(3)
+    col2.image(image2, caption='Immunofluorescence staining with IgG in a patient with diabetic nephropathy ')
+    st.markdown('3)	Diabetic neuropathy : In diabetes, a complex array of metabolic and vascular factors shift the balance between nerve fiber damage and nerve fiber repair in favor of damage [2]. This occurs in a fiber-selective pattern that preferentially affects the more vulnerable distal sensory and autonomic fibers, leading to the progressive loss of sensation that underlies the clinical manifestations of diabetic polyneuropathy.  ')
+    image3 = Image.open('trial_files/image003.jpg')
+    col1, col2, col3 = st.columns(3)
+    col2.image(image3, caption='Loss of pain and other sensations in foot')
+    st.markdown(' 4)Diabetic foot Ulcers : Chronic ulceration in patients with diabetes is multifactorial, due to a combination of diabetic neuropathy, autonomic dysfunction, and vascular insufficiency. Nonischemic diabetic foot ulcers are due to a combination of foot deformities and neuropathy preventing the sensation of pain in areas of the foot that are traumatized. ')
+    image4 = Image.open('trial_files/image004.jpg')
+    col1, col2, col3 = st.columns(3)
+    col2.image(image4, caption='Representation of ulcer in base of foot')
+    st.markdown('5)	Macrovascular complications: Increases the risk of macrovascular complications like stroke, coronary artery disease, peripheral vascular disease. Chronic Hyperglycemia causes increased oxidative stress, protein kinase activation, and activation of the receptor for advanced glycation end products, factors that act on the endothelium of blood vessels.  ')
+    image5 = Image.open('trial_files/image005.jpg')
+    image6 = Image.open('trial_files/image006.jpg')
+    image7 = Image.open('trial_files/image007.jpg')
+    col1, col2, col3 = st.columns(3)
+    col1.image(image6, caption='Stroke')
+    col2.image(image5, caption='Coronary Artery Disease')
+    col3.image(image7, caption='Peripheral Vascular Disease')
 
-    st.markdown('This combined with limited access to healthcare facilities has led to a massive underestimation of the prevalence of diabetes in developing nations. Studies have shown that one in two people living with diabetes are unaware of their condition. This is especially dangerous as chronic uncontrolled diabetes can manifest itself as several irreversible and serious complications with lifelong impacts.')
 
     st.markdown('This approach does not require any blood investigations and is therefore widely deployable with minimal infrastructural requirements.')
 
 
 from streamlit_extras.switch_page_button import switch_page
 
-want_to_predict = st.button("I want to check my risk of diabetes!")
+
+st.markdown(' ')
+st.markdown(' ')
+st.markdown(' ')
+
+col1, col2, col3 = st.columns(3)
+
+with col2:
+    want_to_predict = st.button("I want to check my risk of being diabetic!")
+
 if want_to_predict:
     switch_page("ml_page")
 
-    
+st.markdown(' ')
+st.markdown(' ')
 
+
+col1, col2 = st.columns(2)
+
+with col1:
+    want_know_global = st.button("I want to know more about the global scale of this problem")
+
+if want_know_global:
+    switch_page("global_pop")
+
+with col2:
+    prev_diab = st.button("I would like to learn more about how to prevent diabetes.")
+
+if prev_diab:
+    switch_page("prevent_diab")
--- pages/1_ml_page.py
+++ pages/1_ml_page.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pickle
 import numpy as np
 import sklearn
+from streamlit_extras.switch_page_button import switch_page
 
 header = st.container()
 model_training = st.container()
@@ -10,28 +11,30 @@ model_training = st.container()
 
 
 with header:
-    st.title('Predict the risk of having diabetes based on your symptoms')
-    st.markdown('Note: This algorithm is more specifically meant to screen untested individuals to check for the presence of diabtes based on ')
+    st.markdown("<h1 style='text-align: center; color: SkyBlue;'>Predict the risk of having diabetes based on your symptoms</h1>", unsafe_allow_html=True)
+    st.markdown('  ')
+    st.markdown('  ')
+    st.markdown('Note: This algorithm is specifically meant to screen untested individuals (i.e individuals who have not done RBS, HbA1c blood tests) to check for the presence of diabetes based on symptoms associated with chronic hyperglycemia ( seen in undiagnosed, untreated individuals).')
 
 
 
 
 with model_training:
-    st.header('Time to train the model')
+    st.markdown("<h4 style='text-align: center; color: black;'>Please answer the following questions:</h4>", unsafe_allow_html=True)
 
     
     Age = st.text_input("Enter your Age ","Type Here")
-    Polyuria = st.selectbox('Polyuria',['Yes','No'])
-    Polydipsia= st.selectbox('Polydipsia',['Yes','No'])
-    sudden_weight_loss = st.selectbox('sudden_weight_loss',['Yes','No'])
-    weakness= st.selectbox('weakness',['Yes','No'])
-    Polyphagia= st.selectbox('Polyphagia',['Yes','No'])
-    Genital_thrush= st.selectbox('Genital_thrush',['Yes','No'])
-    visual_blurring= st.selectbox('visual_blurring',['Yes','No'])
-    Irritability= st.selectbox('Irritability',['Yes','No'])
-    partial_paresis= st.selectbox('partial_paresis',['Yes','No'])
-    muscle_stiffness= st.selectbox('muscle_stiffness',['Yes','No'])
-    Obesity= st.selectbox('Obesity',['Yes','No'])
+    Polyuria = st.selectbox('Have you experienced an increase in the frequency and duration of voiding of urine? ( greater than 7 times per day)',['Yes','No'])
+    Polydipsia= st.selectbox('Have you experienced persistent and unexplained thirst, regardless of how much water/ fluids you drink?',['Yes','No'])
+    sudden_weight_loss = st.selectbox('Have you undergone a 3-5 kg weight loss within 6 months, without changing your diet or exercise routine? ',['Yes','No'])
+    weakness= st.selectbox('Are you suffering from chronic fatigue (decreased level of physical endurance, low levels of energy or tiredness for long durations)',['Yes','No'])
+    Polyphagia= st.selectbox('Do you experience constantly extreme, insatiable hunger? ',['Yes','No'])
+    Genital_thrush= st.selectbox('Do you experience recurrent episodes of itching and irritation in the genital region?',['Yes','No'])
+    visual_blurring= st.selectbox('Have you noticed your vision being either always blurry, sometimes burry, or occasionally blurry (out of focus) ?',['Yes','No'])
+    Irritability= st.selectbox('Do you experience constant mood swings, bouts of irritability , confusion or aggressiveness?',['Yes','No'])
+    partial_paresis= st.selectbox('Do you have recurrent episodes of nausea,bloating,vomiting, abdominal discomfort ?',['Yes','No'])
+    muscle_stiffness= st.selectbox('Have you experienced persistent muscle cramps or stiffness of muscles?',['Yes','No'])
+    Obesity= st.selectbox('Is your body mass index(BMI) greater than 30? (Google "BMI calculator" and enter weight and height)',['Yes','No'])
 
 Polyuria = 1 if Polyuria == "Yes" else 0
 Polydipsia = 1 if Polydipsia == "Yes" else 0
@@ -54,13 +57,20 @@ def pred_diab( Age,Polyuria,Polydipsia,sudden_weight_loss,weakness,Polyphagia,Ge
     prediction2 =modellr.predict_proba(input)
     return (prediction1,prediction2)
 
-if st.button("Predict diabetes"):
+st.markdown('  ')
+st.markdown('  ')
+col1, col2, col3 = st.columns(3)
+
+with col2:
+    if st.button("Predict diabetes"):
         output1,output2 = pred_diab(Age,Polyuria,Polydipsia,sudden_weight_loss,weakness,Polyphagia,Genital_thrush,visual_blurring,Irritability,partial_paresis,muscle_stiffness,Obesity)
         if output1 == 1:
              outputp = "Diabetic"
         else:
              outputp = "Non-Diabetic"
+        
+        
         st.success('You are likely {}'.format(outputp))
-        st.success('You are likely {}'.format(output2[::,1]))
+        st.success('Your probability of being diabetic is {} (MAX value is 1)'.format(output2[::,1]))
 
 
